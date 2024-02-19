a1b544a6696beb1e1e92c34c45dd283f68d67c5f
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-19 14:01:57 +0100

    Snack 1

---
+++ Snack1/index.php
@@ -0,0 +1,40 @@
+<?php 
+//snack 1 
+$matches = [
+    'match1' => [   'match' => 'Olimpia Milano - Cantù',
+        'hometeam' => 'Olimpia Milano',
+        'awayteam'=> 'Cantù',
+        'homescore' => 55,
+        'awayscore' => 60
+],
+'match2' => [   'match' => 'Miami Heat - Chicago Bulls',
+    'hometeam' => 'Miami Heat',
+    'awayteam'=> 'Chicago Bulls',
+    'homescore' => 102,
+    'awayscore' => 93
+],
+'match3' => [   'match' => 'Dallas Mavericks - Los Angeles Lakers',
+'hometeam' => 'Dallas Mavericks',
+'awayteam'=> 'Los Angeles Lakers',
+'homescore' => 70,
+'awayscore' => 98
+]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Snack 1 -->
+    <h3><strong>Matches</strong></h3>
+    <ul>
+    <?php foreach($matches as $match):   ?>
+       <li><?= $match['match']?> || <?= $match['homescore']?> - <?= $match['awayscore']?></li>
+    <?php endforeach; ?>
+    </ul>
+</body>
+</html>
